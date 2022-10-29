f43aa77285653186ed0a501262cf1ec75478031d
Sina Hernandez <herjaredsinai@gmail.com>
2022-10-29 17:28:33 -0400

    Update webpack config and setup development tools

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports={
+    mode:'development',
+    entry:{
+        index:'./src/index.js'
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            title:'Restaurant'
+        })
+    ],
+    devtool: 'inline-source-map',
+    module:{
+        rules: [
+            {
+                test:/\.css$/,
+                use:['style-loader','css-loader']
+            }
+        ]
+    },
+    devServer:{
+        static:'./dist',
+        hot:true
+    },
+    output:{
+        filename:'[name].bundle.js',
+        path:path.resolve(__dirname,'dist'),
+        clean:true,
+    },
+    optimization:{
+        runtimeChunk:'single'
+    }
+}
